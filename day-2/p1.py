d26f8b6b1b37e22e8b6b56a4b74286231a4c790b
Miraz Zaidi <miraz.zaidi@gmail.com>
2022-12-02 13:23:06 +0100

    soln: day-2 solution - both parts

---
+++ day-2/p1.py
@@ -0,0 +1,29 @@
+from utils.utils import get_lines_from_file
+
+LOSE, DRAW, WIN = 0, 3, 6
+ROCK, PAPER, SCISSOR = 1, 2, 3
+
+
+player_guide = {'X': ROCK, 'Y': PAPER, 'Z': SCISSOR}
+
+opponent_guide = {'A': ROCK, 'B': PAPER, 'C': SCISSOR}
+
+total_score = 0
+
+test_input = get_lines_from_file('day-2/input.txt')
+for row in test_input:
+    opponent_choice, player_choice = row.split()
+    score = 0
+    score = player_guide[player_choice]
+
+    if player_guide[player_choice] == opponent_guide[opponent_choice]:
+        score += DRAW
+    elif any(
+        (player_guide[player_choice] == ROCK and opponent_guide[opponent_choice] == SCISSOR),
+        (player_guide[player_choice] == SCISSOR and opponent_guide[opponent_choice] == PAPER),
+        (player_guide[player_choice] == PAPER and opponent_guide[opponent_choice] == ROCK),
+    ):
+        score += WIN
+    total_score += score
+
+print(total_score)
---
+++ day-2/p2.py
@@ -0,0 +1,40 @@
+from utils.utils import get_lines_from_file
+
+
+LOSE, DRAW, WIN = 0, 3, 6
+ROCK, PAPER, SCISSOR = 1, 2, 3
+
+
+winner_combo = {ROCK: PAPER, PAPER: SCISSOR, SCISSOR: ROCK}
+
+loser_combo = {
+    ROCK: SCISSOR,
+    PAPER: ROCK,
+    SCISSOR: PAPER,
+}
+
+opponent_guide = {'A': ROCK, 'B': PAPER, 'C': SCISSOR}
+
+player_guide = {'X': LOSE, 'Y': DRAW, 'Z': WIN}
+
+total_score = 0
+
+test_input = get_lines_from_file('day-2/input.txt')
+
+for row in test_input:
+    opponent_choice, player_choice = row.split()
+    score = 0
+    if player_guide[player_choice] == DRAW:
+        score += DRAW
+        score += opponent_guide[opponent_choice]
+
+    elif player_guide[player_choice] == WIN:
+        score += WIN
+        score += winner_combo[opponent_guide[opponent_choice]]
+
+    elif player_guide[player_choice] == LOSE:
+        score += LOSE
+        score += loser_combo[opponent_guide[opponent_choice]]
+    total_score += score
+
+print(total_score)
